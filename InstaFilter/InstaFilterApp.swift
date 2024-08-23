f4b1d5bc93080ca3efa7d9e7816eb3f487f6136e
Mayur <mayurtyagi@unolo.com>
2024-08-23 19:32:35 +0530

    Initial Commit

---
+++ InstaFilter/InstaFilterApp.swift
@@ -0,0 +1,17 @@
+//
+//  InstaFilterApp.swift
+//  InstaFilter
+//
+//  Created by Mayur  on 23/08/24.
+//
+
+import SwiftUI
+
+@main
+struct InstaFilterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
