54f2befaf7b72288e83ba0e88cc22589ab0ae931
Mayur <mayurtyagi@unolo.com>
2024-09-01 11:12:01 +0530

    2nd daY

--- InstaFilter/ContentView.swift
+++ InstaFilter/ContentView.swift
@@ -4,36 +4,71 @@
 //
 //  Created by Mayur  on 23/08/24.
 //
-
+import CoreImage
+import CoreImage.CIFilterBuiltins
 import SwiftUI
 
 struct ContentView: View {
-    @State private var showConfirmationAlert = false
-    @State private var backgroundColor = Color.white
+    @State private var image: Image?
+    @State private var amount = 1.0
     
-    var body: some View {
+    var body: some View{
         VStack{
-            Button("tap me") {
-                showConfirmationAlert.toggle()
-            }
-            .background(.white)
-            .frame(maxWidth: 50, maxHeight: .infinity)
-            .background(backgroundColor)
+            Slider(value: $amount, in: 1...1000)
+                .onChange(of: amount) { oldValue, newValue in
+                    loadImage()
+                }
+            image?
+                .resizable()
+                .scaledToFit()
         }
-        .frame(width: 100, height: 100)
-        .confirmationDialog("change background", isPresented: $showConfirmationAlert) {
-            Button("Red") {
-                backgroundColor = .red
-            }
-            Button("Green") {
-                backgroundColor = .green
-            }
-            Button("Blue") {
-                backgroundColor = .blue
-            }
-            Button("cance", role: .cancel) {}
-        }
+        
     }
+    func loadImage(){
+        let inputImage = UIImage(resource: .example)
+        let ciImage = CIImage(image: inputImage)
+        
+        let context = CIContext()
+        let filter = CIFilter.pixellate()
+        
+        filter.inputImage = ciImage
+        
+        let inputKeys = filter.inputKeys
+        
+        if inputKeys.contains(kCIInputScaleKey){
+            filter.setValue(amount, forKey: kCIInputScaleKey)
+        }
+        
+        guard let outputImage = filter.outputImage else {return}
+        guard let cgimage = context.createCGImage(outputImage, from: outputImage.extent) else {return}
+        let uiImage = UIImage(cgImage: cgimage)
+        image = Image(uiImage: uiImage)
+        
+    }
+//    var body: some View {
+//        ContentUnavailableView("No content Available", image: "swift")
+//        
+//        ContentUnavailableView {
+//            Label("No snippets", systemImage: "swift")
+//        } description: {
+//            Text("You don't have any saved snippets yet.")
+//        } actions: {
+//            Button("Create Snippet") {
+//                // create a snippet
+//            }
+//            .buttonStyle(.borderedProminent)
+//        }
+//        
+//        ContentUnavailableView {
+//            Label("no content available", systemImage: "swift")
+//        } description: {
+//            Text("there is no content available")
+//        } actions: {
+//            Button("create snippet") {
+//                print("New snippet created")
+//            }
+//        }
+//    }
 }
 
 #Preview {
