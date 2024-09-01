983064de0029f90807397c66d15a17d19c58873b
Mayur <mayurtyagi@unolo.com>
2024-09-01 12:29:25 +0530

    some share stuff

--- InstaFilter/ContentView.swift
+++ InstaFilter/ContentView.swift
@@ -4,36 +4,14 @@
 //
 //  Created by Mayur  on 23/08/24.
 //
-import PhotosUI
 import SwiftUI
 
 struct ContentView: View {
-    @State private var pickerPhotos = [PhotosPickerItem]()
-    @State private var selectedPhotos = [Image]()
     
     var body: some View{
-        PhotosPicker(selection: $pickerPhotos, maxSelectionCount: 3, matching: .images){
-            Label("select photo from library", systemImage: "photo")
-        }
-        ScrollView{
-            ForEach(0..<selectedPhotos.count, id: \.self){ index in
-                selectedPhotos[index]
-                    .resizable()
-                    .scaledToFit()
-            }
-        }
-        .onChange(of: pickerPhotos) {
-            Task{
-                selectedPhotos.removeAll()
-                for photo in pickerPhotos{
-                    if let loadedImage = try await photo.loadTransferable(type: Image.self) {
-                        selectedPhotos.append(loadedImage)
-                    }
-                }
-            }
-            
-            
-            
+        let image = Image(.example)
+        ShareLink(item: image, preview: SharePreview("hello", image: image)){
+            Label("Tap to share", systemImage: "swift")
         }
     }
 }
