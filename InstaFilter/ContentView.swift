fd282ee65c0b259d9c82c9a50668afab84cffb7b
Mayur <mayurtyagi@unolo.com>
2024-09-01 12:21:29 +0530

    another  chanhge s  come

--- InstaFilter/ContentView.swift
+++ InstaFilter/ContentView.swift
@@ -4,71 +4,38 @@
 //
 //  Created by Mayur  on 23/08/24.
 //
-import CoreImage
-import CoreImage.CIFilterBuiltins
+import PhotosUI
 import SwiftUI
 
 struct ContentView: View {
-    @State private var image: Image?
-    @State private var amount = 1.0
+    @State private var pickerPhotos = [PhotosPickerItem]()
+    @State private var selectedPhotos = [Image]()
     
     var body: some View{
-        VStack{
-            Slider(value: $amount, in: 1...1000)
-                .onChange(of: amount) { oldValue, newValue in
-                    loadImage()
+        PhotosPicker(selection: $pickerPhotos, maxSelectionCount: 3, matching: .images){
+            Label("select photo from library", systemImage: "photo")
+        }
+        ScrollView{
+            ForEach(0..<selectedPhotos.count, id: \.self){ index in
+                selectedPhotos[index]
+                    .resizable()
+                    .scaledToFit()
+            }
+        }
+        .onChange(of: pickerPhotos) {
+            Task{
+                selectedPhotos.removeAll()
+                for photo in pickerPhotos{
+                    if let loadedImage = try await photo.loadTransferable(type: Image.self) {
+                        selectedPhotos.append(loadedImage)
+                    }
                 }
-            image?
-                .resizable()
-                .scaledToFit()
+            }
+            
+            
+            
         }
-        
     }
-    func loadImage(){
-        let inputImage = UIImage(resource: .example)
-        let ciImage = CIImage(image: inputImage)
-        
-        let context = CIContext()
-        let filter = CIFilter.pixellate()
-        
-        filter.inputImage = ciImage
-        
-        let inputKeys = filter.inputKeys
-        
-        if inputKeys.contains(kCIInputScaleKey){
-            filter.setValue(amount, forKey: kCIInputScaleKey)
-        }
-        
-        guard let outputImage = filter.outputImage else {return}
-        guard let cgimage = context.createCGImage(outputImage, from: outputImage.extent) else {return}
-        let uiImage = UIImage(cgImage: cgimage)
-        image = Image(uiImage: uiImage)
-        
-    }
-//    var body: some View {
-//        ContentUnavailableView("No content Available", image: "swift")
-//        
-//        ContentUnavailableView {
-//            Label("No snippets", systemImage: "swift")
-//        } description: {
-//            Text("You don't have any saved snippets yet.")
-//        } actions: {
-//            Button("Create Snippet") {
-//                // create a snippet
-//            }
-//            .buttonStyle(.borderedProminent)
-//        }
-//        
-//        ContentUnavailableView {
-//            Label("no content available", systemImage: "swift")
-//        } description: {
-//            Text("there is no content available")
-//        } actions: {
-//            Button("create snippet") {
-//                print("New snippet created")
-//            }
-//        }
-//    }
 }
 
 #Preview {
